e0014f71648ff43fe9101c49dd6a20079d95f9bb
lawanmaciel <irolawan33@gmail.com>
2020-09-28 01:46:36 -0300

    Atualizando o template

--- index.php
+++ index.php
@@ -1,30 +1,50 @@
+<?php require __DIR__ . '/recursos/template/array.php'; ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
-    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
-    <link rel="stylesheet" href="recursos/css/estilo.css">
-    <title>Curso PHP</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aprendendo - PHP 7</title>
+    <link rel="stylesheet" href="/recursos/css/estilo.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
+
 <body>
-    <header class="cabecalho">
-        <h1>Curso PHP</h1>
-        <h2>Índice dos Exercícios</h2>
-    </header>
-    <main class="principal">
-        <div class="conteudo">
-            <nav class="modulos">
-                <div class="modulo verde">
-                    <h3>Módulo 01</h3>
-                    <ul>
-                        <li><a href="exercicio.php?dir=teste&file=teste">Exercício A</a></li>
-                    </ul>
+    <section id="pagina-principal">
+        <div class="container">
+            <div class="d-flex justify-content-center">
+                <div class="logo">
+                    <h1>PHP</h1>
                 </div>
-            </nav>
+            </div>
+            <div class="modulos-cards">
+                <div class="row">
+                    <?php
+                    foreach ($modulos as $key => $value) {
+                    ?>
+                        <div class="col-4">
+                            <div class="cards-custom" style="border: 3px solid <?= $value['cor'] ?>;">
+                                <div class="row">
+                                    <div class="id-card" style="border: 3px solid <?= $value['cor'] ?>;">
+                                        <h1><?= ($key + 1) ?></h1>
+                                    </div>
+                                    <h1 class="titulo-card"><?= $value['titulo'] ?></h1>
+                                </div>
+                                <div class="conteudo">
+                                    <a href="">
+                                        <?= $value['conteudo'] ?>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    <?php
+                    }
+                    ?>
+                </div>
+            </div>
         </div>
-    </main>
-    <footer class="rodape">
-        COD3R & ALUNOS © <?= date('Y'); ?>
-    </footer>
+    </section>
 </body>
+
 </html>
---
+++ recursos/template/array.php
@@ -0,0 +1,14 @@
+<?php
+        $modulos = [
+            [
+                "titulo" => 'Primeiro módulo',
+                "conteudo" => 'Exercício A',
+                "cor" => '#E52E71',
+            ],
+            [
+                "titulo" => 'Segundo módulo',
+                "conteudo" => 'Exercício B',
+                "cor" => '#6849b6',
+            ],
+        ]
+?>
