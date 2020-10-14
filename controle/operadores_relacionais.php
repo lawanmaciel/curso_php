3474c47164a29099d1ef5f2015529287101dd0af
lawanmaciel <lawan@melhoresescolasmedicas.com>
2020-10-14 17:07:15 -0300

    Subindo novo módulo de estrutura de controle

---
+++ controle/operadores_relacionais.php
@@ -0,0 +1,15 @@
+<div class="titulo">Operadores Relacionais</div>
+
+<?php
+
+// var_dump(1 == 1);
+// var_dump(1 > 1);
+// var_dump(1 >= 1);
+// var_dump(4 < 23);
+// var_dump(1 <= 7);
+// var_dump(1 == 1);
+// var_dump(1 <> 1);
+// var_dump(1 != 1);
+
+var_dump(1 === '1');
+var_dump('sim' === 'sim');
